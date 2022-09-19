4d81a2d52c32cc58212217cbde5c8b0029c07216
Nina Palma <ninapalmadev@gmail.com>
2022-09-19 11:46:12 -0300

    cria métodos com sobrecarga para calculo de area de quadrilateros

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,11 @@
 public class Main {
+    public static void main(String[] args) {
+        //Quadrilateros
+        System.out.println("Exercício sobrecarga - Quadriláteros");
+        Quadrilatero.area(5);
+        Quadrilatero.area(5d,3d);
+        Quadrilatero.area(8,9,10);
+        Quadrilatero.area(5f,3f);
+    }
+
 }
